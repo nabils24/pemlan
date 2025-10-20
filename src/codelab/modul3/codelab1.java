4d61872ef6b48ac5f1de58f7441af763584e53f0
Nabil Sahsada Suratno <nabilsahsadacode@gmail.com>
2025-10-20 09:04:55 +0700

    update file codelab1 dimodul3

---
+++ src/codelab/modul3/codelab1.java
@@ -0,0 +1,30 @@
+/*
+ * ===========================================
+ * Author   : Nabil Sahsada Suratno
+ * Mode     : Java Main Class
+ * Nim      : 202410370110357
+ * ===========================================
+ */
+
+package codelab.modul3;
+
+
+/**
+ * Class codelab1 - Deskripsi singkat mengenai kelas ini.
+ */
+public class codelab1 {
+
+    // Constructor
+    public codelab1() {
+        // Inisialisasi jika diperlukan
+    }
+
+    /**
+     * Metode utama yang dijalankan saat program dimulai.
+     *
+     * @param args Argumen baris perintah
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
